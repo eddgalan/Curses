75baae0ca67c88e4261a7bb3b09ffa67af4cd54c
Edson Galan <edd.galan@hotmail.com>
2025-06-26 22:11:23 -0600

    Manejo de cadenas y operaciones básicas

---
+++ python/CursoDePython/05-Cadenas/app.py
@@ -0,0 +1,20 @@
+name = "Edson"
+lastname = 'Galan'
+fullname = '''Edson
+Galan
+'''
+
+print("Type of 'name' var: " + str(type(name)))
+print("Type of 'characer' var: " + str(type(lastname)))
+print("Type of 'lastname' var: " + fullname)
+
+# Indexation
+print(name[0] + lastname[0])
+print(name[-1] + lastname[-1])
+
+print(name*3)
+
+print(len(name))
+
+print(name.upper())
+print(name.lower())
