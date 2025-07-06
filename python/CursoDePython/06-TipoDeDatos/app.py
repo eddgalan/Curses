2b24ef884f1e0725be5e6cc2c4dea4248c994031
Edson Galan <edd.galan@hotmail.com>
2025-07-06 13:56:54 -0600

    Tipos de Datos en Python

---
+++ python/CursoDePython/06-TipoDeDatos/app.py
@@ -0,0 +1,18 @@
+x = 10
+y = 3.1416
+z = 1.2e6
+a = 1.2e-6
+
+print(type(x))
+print(type(y))
+print('z = ' + str(z) + ' ' + str(type(z)))
+print('a = ' + str(a) + ' ' + str(type(a)))
+
+is_true = True
+is_false = False
+
+print(is_true)
+print(type(is_true))
+
+print(is_false)
+print(type(is_false))
