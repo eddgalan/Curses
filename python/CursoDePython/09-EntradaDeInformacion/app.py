5246fb0e89f54215e9fde7073084a1f5fd007749
Edson Galan <edd.galan@hotmail.com>
2025-07-06 15:28:44 -0600

    Entrada de información en Python

---
+++ python/CursoDePython/09-EntradaDeInformacion/app.py
@@ -0,0 +1,8 @@
+name = input("Type your name:")
+print(f"Hello {name}")
+
+age = int(input("Type your age:"))
+print(f"You are {age} years old")
+
+print("Name is type: ", type(name))
+print("Age is type: ", type(age))
