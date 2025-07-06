0d44365a060711dc45fd522ca6705a303b9eaf3e
Edson Galan <edd.galan@hotmail.com>
2025-07-06 14:58:14 -0600

    Operaciones Matemáticas avanzadas en Python

---
+++ python/CursoDePython/08-OperacionesMateAvanzadas/app.py
@@ -0,0 +1,41 @@
+a = 10
+b = 3
+
+print("a = " + str(a), "b = " + str(b))
+
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+
+print("Multiplicación", a * b)
+print("Potencia: ", a ** b)
+
+print("Division", a / b)
+print("Parte entera de la division", a // b)
+
+print("Modulo: ", a % b)
+
+a -= 2
+print("a - 2 = ", a)
+
+a *= 3
+print("a * 2 ", a)
+
+a /= 3
+print("a / 3 ", a)
+
+"""
+============ PEMDAS =============
+P: Potenciación
+E: Exponenciación
+M: Multiplicación
+D: División
+A: Adición
+S: Sustracción 
+"""
+operation1 = 2 + 3 * 4
+operation2 = (2 + 3) * 4
+print(operation1)
+print(operation2)
+
+print("¿a > b?: ", a > b)
+print("¿a < b?: ", a < b)
