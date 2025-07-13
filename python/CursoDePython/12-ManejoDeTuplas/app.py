5f1a2e09c78d309ddbebb6f11bcd22cca5325be9
Edson Galan <edd.galan@hotmail.com>
2025-07-12 23:13:19 -0600

    Manejo de Matrices y Tuplas en Python

---
+++ python/CursoDePython/12-ManejoDeTuplas/app.py
@@ -0,0 +1,15 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix)
+print(matrix[0])
+
+numbers = (1,2,3,4,5)
+numbers_ = 1,2,3,4,5        #También se pueden declarar así las tuplas
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+# numbers[0] = 10   Error. Las tuplas son inmutables
+
