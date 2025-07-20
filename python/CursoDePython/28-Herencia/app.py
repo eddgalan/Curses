e1397c1d17b6a0371e416c42d0be0955dcb36627
Edson Galan <edd.galan@hotmail.com>
2025-07-19 23:42:08 -0600

    Herencia y uso de función super()

---
+++ python/CursoDePython/28-Herencia/app.py
@@ -0,0 +1,9 @@
+from person import Person
+from student import Student
+
+p1 = Person('Juan', 25)
+p1.greet()
+
+s1 = Student('Pedro', 25, '1234')
+s1.greet()
+
---
+++ python/CursoDePython/28-Herencia/student.py
@@ -0,0 +1,11 @@
+from person import Person
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def greet(self):
+        super().greet()
+        print(f'My student ID is: {self.student_id}')
+
