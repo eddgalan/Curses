58e293285ca7bc4fd7321ca077e7f2ac989b6697
Edson Galan <edd.galan@hotmail.com>
2025-08-02 15:45:48 -0600

    Comentarios y Docstrings

--- python/CursoDePython/31-ManipulacionDeArchivosCSV/csv_reader.py
+++ python/CursoDePython/31-ManipulacionDeArchivosCSV/csv_reader.py
@@ -1,41 +1,76 @@
 import csv
 from datetime import datetime
 
+
 class CsvReader:
-    def __init__(self, filename='products.csv'):
+    def __init__(self, filename="products.csv"):
         self.filename = filename
 
     def __str__(self):
-        return f'Filename: {self.filename}'
+        return f"Filename: {self.filename}"
 
     def show_all_file_content(self):
-        with open(self.filename, mode='r', newline='', encoding='utf-8') as file:
+        """
+        Reads and prints all content from a CSV file specified by the filename attribute.
+
+        :param self: Refers to the instance of the class containing this method. Assumes
+            the filename attribute is set and references a valid CSV file.
+        :return: None
+        """
+        with open(self.filename, mode="r", newline="", encoding="utf-8") as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 print(row)
 
     def show_all_file_content_main_fields(self):
-        with open(self.filename, mode='r', newline='', encoding='utf-8') as file:
+        """
+        Displays the main fields of all rows in a CSV file.
+
+        :param self: The instance of the class containing the filename attribute.
+        :type self: object
+        :raises KeyError: If the `name` or `price` fields are not found in the CSV file.
+        :return: None
+        """
+        with open(self.filename, mode="r", newline="", encoding="utf-8") as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 print(f"Product: {row['name']} | Price: {row['price']}")
 
     def add_new_product(self):
+        """
+        Adds a new product by taking user input and appending the details to a CSV file.
+
+        :raises IOError: if there is an error opening or writing to the file.
+
+        :return: None
+        """
         name = input("Type the product name: ")
         price = input("Type the product price: ")
         quantity = input("Quantity: ")
         brand = input("Type the brand: ")
         category = input("Type the product category: ")
-        entry_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        entry_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        with open(self.filename, mode='a', newline='') as file:
-            csv_writer = csv.DictWriter(file, fieldnames=['name', 'price', 'quantity', 'brand', 'category', 'entry_date'])
-            csv_writer.writerow({
-                'name': name,
-                'price': price,
-                'quantity': quantity,
-                'brand': brand,
-                'category': category,
-                'entry_date': entry_date
-            })
-        print('Product added successfully!')
+        with open(self.filename, mode="a", newline="") as file:
+            csv_writer = csv.DictWriter(
+                file,
+                fieldnames=[
+                    "name",
+                    "price",
+                    "quantity",
+                    "brand",
+                    "category",
+                    "entry_date",
+                ],
+            )
+            csv_writer.writerow(
+                {
+                    "name": name,
+                    "price": price,
+                    "quantity": quantity,
+                    "brand": brand,
+                    "category": category,
+                    "entry_date": entry_date,
+                }
+            )
+        print("Product added successfully!")
--- python/CursoDePython/32-ManejoDeArchivosJSON/json_file_manager.py
+++ python/CursoDePython/32-ManejoDeArchivosJSON/json_file_manager.py
@@ -1,46 +1,82 @@
 import json
 from datetime import datetime
 
+
 class JsonFileManager:
-    def __init__(self, filename='products.json'):
+    def __init__(self, filename="products.json"):
         self.filename = filename
 
     def __str__(self):
-        return f'Filename: {self.filename}'
+        return f"Filename: {self.filename}"
 
     def show_all_file_content(self):
-        with open(self.filename, mode='r', newline='', encoding='utf-8') as file:
+        """
+        Reads and displays the contents of a JSON file stored in the given filename.
+
+        :param self: Represents the instance of the object, allowing access to
+            attributes and other methods within the class.
+        :raises FileNotFoundError: If the file specified by `self.filename` does not
+            exist or cannot be found.
+        :raises json.JSONDecodeError: If the file content is not a valid JSON.
+        """
+        with open(self.filename, mode="r", newline="", encoding="utf-8") as file:
             json_file = json.load(file)
             for product in json_file:
                 print(product)
 
     def show_all_file_content_main_fields(self):
-        with open(self.filename, mode='r', newline='', encoding='utf-8') as file:
+        """
+        Reads the content of a specified JSON file and outputs specific fields
+        ('name' and 'price') for each product listed in the file.
+
+        :param self: Represents the instance of the class.
+
+        :raises FileNotFoundError: If the specified file does not exist.
+        :raises json.JSONDecodeError: If the file content is not valid JSON.
+
+        :return: None
+        """
+        with open(self.filename, mode="r", newline="", encoding="utf-8") as file:
             json_file = json.load(file)
             for product in json_file:
                 print(f"Product: {product['name']} | Price: {product['price']}")
 
     def add_new_product(self):
+        """
+        Adds a new product to the product list stored in a JSON file.
+
+        This method prompts the user for the product's name, price, quantity, brand,
+        and category.
+
+        :raises FileNotFoundError:
+            If the file specified by `self.filename` does not exist.
+        :raises json.JSONDecodeError:
+            If the file contains invalid JSON.
+        :param self: Instance of the class that invokes this method (assumes the
+            instance has a `filename` attribute indicating the path to the JSON file).
+
+        :return: None
+        """
         name = input("Type the product name: ")
         price = input("Type the product price: ")
         quantity = input("Quantity: ")
         brand = input("Type the brand: ")
         category = input("Type the product category: ")
-        entry_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        entry_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         new_product = {
-            'name': name,
-            'price': price,
-            'quantity': quantity,
-            'brand': brand,
-            'category': category,
-            'entry_date': entry_date
+            "name": name,
+            "price": price,
+            "quantity": quantity,
+            "brand": brand,
+            "category": category,
+            "entry_date": entry_date,
         }
 
-        with open(self.filename, mode='r') as file:
+        with open(self.filename, mode="r") as file:
             products = json.load(file)
         products.append(new_product)
 
-        with open(self.filename, mode='w') as file:
+        with open(self.filename, mode="w") as file:
             json.dump(products, file, indent=4)
-        print('Product added successfully!')
+        print("Product added successfully!")
