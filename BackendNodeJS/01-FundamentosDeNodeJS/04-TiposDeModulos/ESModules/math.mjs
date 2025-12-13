b2e6e13f7d5544662f446d93050273bddf3ec4a8
Edson Galan <edd.galan@hotmail.com>
2025-12-13 17:11:37 -0600

    Tipos de modulos en NodeJS: CommonJS y ESModules

---
+++ BackendNodeJS/01-FundamentosDeNodeJS/04-TiposDeModulos/ESModules/math.mjs
@@ -0,0 +1,7 @@
+export function multiply(x, y) {
+  return x * y;
+}
+
+export function divide(x, y) {
+  return x / y;
+}
---
+++ BackendNodeJS/01-FundamentosDeNodeJS/04-TiposDeModulos/native.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./04-TiposDeModulos/example.txt', 'utf-8');
+console.log('File Contents: ', data);
