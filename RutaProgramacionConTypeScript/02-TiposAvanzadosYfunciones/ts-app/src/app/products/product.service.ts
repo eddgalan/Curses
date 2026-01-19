9338ca2b3b075ca3360bcf4e306a1101610e1abf
Edson Galan <edd.galan@hotmail.com>
2026-01-18 21:17:52 -0600

    15 Propiedades de solo lectura

--- RutaProgramacionConTypeScript/02-TiposAvanzadosYfunciones/ts-app/src/app/products/product.service.ts
+++ RutaProgramacionConTypeScript/02-TiposAvanzadosYfunciones/ts-app/src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import { type Product} from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+//    data.id = Math.random();
+//    data.createdAt = new Date();
     products.push(data);
 };
