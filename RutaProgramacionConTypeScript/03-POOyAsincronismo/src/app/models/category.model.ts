7f6a68319c64cbefe7d8ee1ff5fab6e1d1cea463
Edson Galan <edd.galan@hotmail.com>
2026-03-08 17:50:37 -0600

    POO | 17 De funciones a clases

--- RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/models/category.model.ts
+++ RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/models/category.model.ts
@@ -1,8 +1,5 @@
 export interface Category {
   id:         number;
   name:       string;
-  slug:       string;
   image:      string;
-  creationAt: Date;
-  updatedAt:  Date;
 }
---
+++ RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/services/product-memory.service.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { type Product } from '../models/product.model';
+import {type CreateProductDto, type UpdateProductDto} from "../dtos/product.dto.js";
+
+export class ProductMemoryService {
+  private products: Product[] = [];
+
+  create(data: CreateProductDto) {
+    const newProduct = {
+      ...data,
+      id: faker.number.int(),
+      category: {
+        id: data.categoryId,
+        name: faker.commerce.department(),
+        image: faker.image.url(),
+      }
+    };
+    return this.add(newProduct);
+  };
+
+  add(product: Product) {
+    this.products.push(product);
+    return product;
+  }
+
+  update(id: Product['id'], changes: UpdateProductDto): Product {
+    const index = this.products.findIndex(product => product.id === id);
+    const prevData = this.products[index];
+
+    this.products[index] = <Product> {
+      ...prevData,
+      ...changes
+    };
+
+    return this.products[index];
+  }
+
+  get(id: Product['id']) {
+    return this.products.find(product => product.id === id);
+  }
+}
+
+export const products: Product[] = [];
