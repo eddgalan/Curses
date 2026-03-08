cf0f04f5a800e2747c6f6700a1876fa47492c069
Edson Galan <edd.galan@hotmail.com>
2026-03-08 17:50:14 -0600

    POO | 16 Tipado respuestas HTTP

---
+++ RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+import type { Category } from './category.model';
+
+export interface Product {
+  id:          number;
+  title:       string;
+  slug:        string;
+  price:       number;
+  description: string;
+  category:    Category;
+  images:      string[];
+  creationAt:  Date;
+  updatedAt:   Date;
+}
