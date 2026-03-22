ac73ad76de0d63ec2778cc444fe140c41e9cb298
Edson Galan <edd.galan@hotmail.com>
2026-03-22 17:27:39 -0600

    Genericos | 24 Decoradores

--- RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/models/category.model.ts
+++ RutaProgramacionConTypeScript/03-POOyAsincronismo/src/app/models/category.model.ts
@@ -1,5 +1,11 @@
+export enum ACCESS_TYPE {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+}
+
 export interface Category {
   id:         number;
   name:       string;
   image:      string;
+  access?:     ACCESS_TYPE;
 }
