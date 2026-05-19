a721c668a257a8980903526ea4ceceb286e83c2a
Edson Galan <edd.galan@hotmail.com>
2026-05-19 16:48:55 -0600

    Curso de React.js | Que es el estado

--- Frontend/curso-react-intro-main/src/TodoSearch.js
+++ Frontend/curso-react-intro-main/src/TodoSearch.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('buscando: ', searchValue, '')
+
   return (
     <input
-      placeholder="Buscar tarea..."
       className="TodoSearch"
-      onChange={ (event) => {
-        console.log('Input event', event.target.value);
+      placeholder="Buscar tarea..."
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
       }}
     />
   );
