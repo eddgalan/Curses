2dafb4a2ba3366ace16096414e3b41f586ae3ba1
Edson Galan <edd.galan@hotmail.com>
2026-05-25 20:31:16 -0600

    Curso de React.js | Que es React Context

--- Frontend/curso-react-intro-main/src/App/AppUI.js
+++ Frontend/curso-react-intro-main/src/App/AppUI.js
@@ -7,49 +7,40 @@ import React from 'react';
 import {TodosLoading} from "../TodosLoading";
 import {TodosError} from "../TodosError";
 import {TodosEmpty} from "../TodosEmpty";
+import {TodoContext} from "../TodoContext";
 
-function AppUI({
-                 loading,
-                 error,
-                 completedTodos,
-                 totalTodos,
-                 searchValue,
-                 setSearchValue,
-                 searchedTodos,
-                 completeTodo,
-                 deleteTodo
- }) {
+function AppUI() {
   return (
     <React.Fragment>
-      <TodoCounter
-        totalTodos={totalTodos}
-        completedTodos={completedTodos}/>
-      <TodoSearch
-        serchValue={searchValue}
-        setSearchValue={setSearchValue}/>
-      <TodoList>
-        {loading && (<>
-          <TodosLoading /><TodosLoading /><TodosLoading />
-        </>)}
-        {error && <TodosError />}
-        {(!loading && searchedTodos.length === 0) && <TodosEmpty />}
+      <TodoCounter />
+      <TodoSearch />
+      <TodoContext.Consumer>
+        {({loading, error, searchedTodos, completeTodo, deleteTodo}) => (
+          <TodoList>
+            {loading && (<>
+              <TodosLoading /><TodosLoading /><TodosLoading />
+            </>)}
+            {error && <TodosError />}
+            {(!loading && searchedTodos.length === 0) && <TodosEmpty />}
 
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            /*
-            Se puede tambien proner la propiedad onComplete de la siguiente forma:
-             onComplete={completeTodo}
-             Y en el TodoItem poner el onClick asi:
-             onClick={props.onComplete.bind(null, props.text)}
-             */
-            onDelete={deleteTodo}
-          />
-        ))}
-      </TodoList>
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                /*
+                Se puede tambien proner la propiedad onComplete de la siguiente forma:
+                 onComplete={completeTodo}
+                 Y en el TodoItem poner el onClick asi:
+                 onClick={props.onComplete.bind(null, props.text)}
+                 */
+                onDelete={deleteTodo}
+              />
+            ))}
+          </TodoList>
+        )}
+      </TodoContext.Consumer>
       <CreateTodoButton/>
     </React.Fragment>
   );
--- Frontend/curso-react-intro-main/src/App/index.js
+++ Frontend/curso-react-intro-main/src/App/index.js
@@ -1,62 +1,12 @@
 import React from 'react';
+import {TodoProvider} from '../TodoContext';
 import {AppUI} from './AppUI';
-import {useLocalStorage} from './UseLocalStorage';
 
 function App() {
-  const {
-    item:todos,
-    saveItem:saveTodos,
-    loading,
-    error
-  } = useLocalStorage('TODOS_V1', []);
-
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const completedTodos = todos.filter(todo => todo.completed).length;
-  const totalTodos = todos.length;
-
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
-
-  const getTodoIndexByText = (text) => {
-    return todos.findIndex(
-      (todo) => todo.text === text
-    );
-  }
-
-  const completeTodo = (text) => {
-    const index = getTodoIndexByText(text);
-    const newTodos = [...todos];
-
-    newTodos[index].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const index = getTodoIndexByText(text);
-    const newTodos = [...todos];
-
-    newTodos.splice(index, 1);
-    saveTodos(newTodos);
-  };
-
   return (
-    <AppUI
-      loading={loading}
-      error={error}
-      completedTodos={completedTodos}
-      totalTodos={totalTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   )
 }
 
