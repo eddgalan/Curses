4486ac7ffcdd14d2df540dcd2ea01bf6e0b5634a
Edson Galan <edd.galan@hotmail.com>
2026-06-26 15:07:19 -0600

    Curso de ReactJS TS | useRef

--- Frontend/curso-de-react-ts/next-app/components/FoxGallery/RandomFox.tsx
+++ Frontend/curso-de-react-ts/next-app/components/FoxGallery/RandomFox.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 
 type Props = { imageUrl: string };
 
 export const RandomFox = ({ imageUrl }: Props ): React.JSX.Element => {
+    const node = useRef<HTMLImageElement>(null);
+
     return (
         <img src={ imageUrl }
              alt="Fox image"
              width={320}
              className="h-auto rounded-lg"
+             ref={node}
         />
     );
 };
