6e0d083b900bbccef0d96d37910d28b2756ab94d
Edson Galan <edd.galan@hotmail.com>
2026-06-26 16:37:28 -0600

    Curso de ReactJS TS | Lazy loading con Interseccion Observer

--- Frontend/curso-de-react-ts/next-app/components/FoxGallery/RandomFox.tsx
+++ Frontend/curso-de-react-ts/next-app/components/FoxGallery/RandomFox.tsx
@@ -1,15 +1,37 @@
-import React, {useRef} from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 type Props = { imageUrl: string };
 
 export const RandomFox = ({ imageUrl }: Props ): React.JSX.Element => {
     const node = useRef<HTMLImageElement>(null);
+    const [src, setSrc] = useState<string>("data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==");
+
+    useEffect(() => {
+        if (!node.current) return;
+
+        // Create a new IntersectionObserver instance
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setSrc(imageUrl);
+                }
+            });
+        });
+
+        // Start observing the target node
+        observer.observe(node.current);
+
+        // Stop observing the target node
+        return () => {
+            observer.disconnect();
+        }
+    }, [imageUrl]);
 
     return (
-        <img src={ imageUrl }
+        <img src={ src }
              alt="Fox image"
              width={320}
-             className="h-auto rounded-lg"
+             className="h-auto rounded-lg bg-gray-300"
              ref={node}
         />
     );
