7098670b283a40f1881ef21296b2cd682c7bec43
Edson Galan <edd.galan@hotmail.com>
2026-06-25 20:52:22 -0600

    Curso de ReactJS TS | Refactorizancion de tipos en TypeScript y React

--- Frontend/curso-de-react-ts/next-app/components/FoxGalery/index.tsx
+++ Frontend/curso-de-react-ts/next-app/components/FoxGalery/index.tsx
@@ -3,25 +3,28 @@
 import { useEffect, useState } from "react";
 import { RandomFox } from "@/components/FoxGalery/RandomFox";
 
+type ImageItem = { id: string, url: string }
+
 export function FoxGallery() {
-    const [images, setImages] = useState<string[]>([]);
+    const [images, setImages] = useState<Array<ImageItem>>([]);
 
     useEffect(() => {
         const random = () => Math.floor(Math.random() * 123) + 1;
+        const generateId = () => Math.random().toString(36).substring(2, 9);
 
         setImages([
-            `https://randomfox.ca/images/${random()}.jpg`,
-            `https://randomfox.ca/images/${random()}.jpg`,
-            `https://randomfox.ca/images/${random()}.jpg`,
-            `https://randomfox.ca/images/${random()}.jpg`,
+            {id: generateId(), url: `https://randomfox.ca/images/${ random() }.jpg`},
+            {id: generateId(), url: `https://randomfox.ca/images/${ random() }.jpg`},
+            {id: generateId(), url: `https://randomfox.ca/images/${ random() }.jpg`},
+            {id: generateId(), url: `https://randomfox.ca/images/${ random() }.jpg`},
         ]);
     }, []);
 
     return (
         <>
-            {images.map((image, index) => (
-                <div key={index} className="p-4">
-                    <RandomFox imageUrl={image} />
+            {images.map(({ id, url }) => (
+                <div key={ id } className="p-4">
+                    <RandomFox imageUrl={ url } />
                 </div>
             ))}
         </>
