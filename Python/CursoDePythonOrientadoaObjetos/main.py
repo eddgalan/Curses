62aa83b2c4f94d8cf15ccfe63305a019def6f335
Edson Galan <edd.galan@hotmail.com>
2026-07-30 20:49:46 -0600

    Curso de Python Orientado a Objetos| 3 Instance Methods That Make python Objects Act

--- Python/CursoDePythonOrientadoaObjetos/main.py
+++ Python/CursoDePythonOrientadoaObjetos/main.py
@@ -7,12 +7,28 @@ class Book:
         self.available = available
 
     def __str__(self):
-        return f"Libro: {self.title} - {self.author}"
+        return f"Book: {self.title} - {self.author} - {self.isbn} - Available: {self.available}"
+
+    def lend_book(self):
+        if self.available:
+            self.available = False
+            print(f"Libro {self.title} prestado")
+        else:
+            print(f"Libro {self.title} no disponible")
+
+    def change_availability(self):
+        self.available = not self.available
 
 my_book = Book("100 anios de soledad", "Gabriel Garcia Marquez", "1234567890", True)
-my_book_ = Book("El principito", "Saint-Exupery", "1234567891", False)
+my_book_ = Book("El principito", "Saint-Exupery", "1234567891", True)
 
 collection = [my_book, my_book_]
 
 for book in collection:
     print(book)
+
+my_book.change_availability()
+print(my_book)
+
+my_book.lend_book()
+my_book_.lend_book()
