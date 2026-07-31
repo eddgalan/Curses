513af1a4c44d8c5175307cca45a2aceb0022aff5
Edson Galan <edd.galan@hotmail.com>
2026-07-30 21:12:09 -0600

    Curso de Python Orientado a Objetos| 4 Private Attributes and Data Integrity in Python

--- Python/CursoDePythonOrientadoaObjetos/main.py
+++ Python/CursoDePythonOrientadoaObjetos/main.py
@@ -1,23 +1,35 @@
 
 class Book:
     def __init__(self, title, author, isbn, available: bool):
-        self.title = title
-        self.author = author
-        self.isbn = isbn
-        self.available = available
+        self._title = title
+        self._author = author
+        self._isbn = isbn
+        self._available = available
 
     def __str__(self):
-        return f"Book: {self.title} - {self.author} - {self.isbn} - Available: {self.available}"
+        return f"Book: {self._title} - {self._author} - {self._isbn} - Available: {self._available}"
 
-    def lend_book(self):
-        if self.available:
-            self.available = False
-            print(f"Libro {self.title} prestado")
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def available(self):
+        return self._available
+
+    @available.setter
+    def available(self, value):
+        self._available = value
+
+    def borrow_book(self):
+        if self._available:
+            self._available = False
+            print(f"Libro {self._title} prestado")
         else:
-            print(f"Libro {self.title} no disponible")
+            print(f"Libro {self._title} no disponible")
 
     def change_availability(self):
-        self.available = not self.available
+        self._available = not self._available
 
 my_book = Book("100 anios de soledad", "Gabriel Garcia Marquez", "1234567890", True)
 my_book_ = Book("El principito", "Saint-Exupery", "1234567891", True)
@@ -30,5 +42,10 @@ for book in collection:
 my_book.change_availability()
 print(my_book)
 
-my_book.lend_book()
-my_book_.lend_book()
+my_book.borrow_book()
+my_book_.borrow_book()
+
+print(my_book.title)
+print(my_book.available)
+my_book.available = True
+print(my_book.available)
