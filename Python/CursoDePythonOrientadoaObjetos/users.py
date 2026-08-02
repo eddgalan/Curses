aa62f5846b38f6560081393a63a5f454e8699187
Edson Galan <edd.galan@hotmail.com>
2026-08-02 16:33:30 -0600

    Curso de Python Orientado a Objetos| 6 Simple Inheritance with Python Classes

---
+++ Python/CursoDePythonOrientadoaObjetos/users.py
@@ -0,0 +1,46 @@
+class User:
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+        self.books_borrowed = []
+
+    def borrow_book(self, book):
+        return f"El usuario {self.name} ha alquilado el libro {book.title}"
+
+
+class Student(User):
+    def __init__(self, name, user_id, course):
+        super().__init__(name, user_id)
+        self.course = course
+        self.books_limit = 3
+
+    def borrow_book(self, title):
+        if len(self.books_borrowed) < self.books_limit:
+            self.books_borrowed.append(title)
+            return f"El usuario {self.name} ha alquilado el libro {title}"
+        else:
+            return f"No puedes alquilar mas libros, limite alcanzado. Limite: {self.books_limit}"
+
+
+class Teacher(User):
+    def __init__(self, name, user_id):
+        super().__init__(name, user_id)
+        self.books_limit = None
+
+    def borrow_book(self, title):
+        self.books_borrowed.append(title)
+        return f"El usuario {self.name} ha alquilado el libro {title}"
+
+
+student = Student("Juan", 123, "Python")
+teacer = Teacher("Pedro", 456)
+
+print(student.borrow_book("Python para iniciantes"))
+print(student.borrow_book("Python para iniciantes"))
+print(student.borrow_book("Python para iniciantes"))
+print(student.borrow_book("Python para iniciantes"))
+
+print(teacer.borrow_book("Python para expertos"))
+print(teacer.borrow_book("Python para expertos"))
+print(teacer.borrow_book("Python para expertos"))
+print(teacer.borrow_book("Python para expertos"))
