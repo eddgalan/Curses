6e5c958a6346599422478f3e933709c0c1c989e2
Edson Galan <edd.galan@hotmail.com>
2026-08-04 17:30:20 -0600

    Curso de Django | 10  Relaciones de Tablas uno a Muchos

--- Python/CursoDjango/my_first_project/my_first_app/models.py
+++ Python/CursoDjango/my_first_project/my_first_app/models.py
@@ -8,3 +8,17 @@ class Car(models.Model):
 
     def __str__(self):
         return self.title
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
