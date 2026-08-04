155ed4e803419ab712a4e4275456e1d0c7a07ba2
Edson Galan <edd.galan@hotmail.com>
2026-08-04 17:03:14 -0600

    Curso de Django | 08 Creacion y gestion de campos en tablas de Django ORM

--- Python/CursoDjango/my_first_project/my_first_app/models.py
+++ Python/CursoDjango/my_first_project/my_first_app/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.CharField(max_length=100)
+    year = models.CharField(max_length=4, null=True)
+    color = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.title
