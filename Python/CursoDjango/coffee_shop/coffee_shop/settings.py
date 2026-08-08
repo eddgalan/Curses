a91f9ee099f89917d86be28960e81cdca9d237f3
Edson Galan <edd.galan@hotmail.com>
2026-08-08 01:26:10 -0600

    Curso de Django | 29 Configuracion de base de datos

--- Python/CursoDjango/coffee_shop/coffee_shop/settings.py
+++ Python/CursoDjango/coffee_shop/coffee_shop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'coffee_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coffee_shop',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'coffee_shop',
+        'PASSWORD': '12345678a',
     }
 }
 
