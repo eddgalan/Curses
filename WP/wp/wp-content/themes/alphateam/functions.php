498195466540b5773ae4900e4fad1c0ff7eadecf
Edson Galan <edd.galan@hotmail.com>
2026-09-10 01:03:59 -0600

    Enqueue Tailwind CSS via CDN and clean up redundant script in header.php

--- WP/wp/wp-content/themes/alphateam/functions.php
+++ WP/wp/wp-content/themes/alphateam/functions.php
@@ -2,7 +2,23 @@
 
 add_action('wp_enqueue_scripts', 'alphateam_enqueue_scripts');
 
-function alphateam_enqueue_scripts() {
-    wp_enqueue_style('alphateam-style', get_stylesheet_directory_uri() . '/style.css');
-    wp_enqueue_script('alphateam-script', get_template_directory_uri() . '/assets/js/header.js', [], '1.0', true);
+function alphateam_enqueue_scripts(): void
+{
+    wp_enqueue_style(
+        'alphateam-style',
+        get_stylesheet_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_script(
+        'alphateam-tailwind-css',
+        'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
+    );
+
+    wp_enqueue_script(
+        'alphateam-script',
+        get_template_directory_uri() . '/assets/js/header.js',
+        [],
+        '1.0',
+        true
+    );
 }
