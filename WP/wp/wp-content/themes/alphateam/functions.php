61feae85e392cba5c48be2da21ef43ab368e34ec
Edson Galan <edd.galan@hotmail.com>
2026-09-09 18:51:29 -0600

    Enqueue theme styles and add wp_head to the header template

---
+++ WP/wp/wp-content/themes/alphateam/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'alphateam_enqueue_scripts');
+
+function alphateam_enqueue_scripts() {
+    wp_enqueue_style('alphateam-style', get_stylesheet_directory_uri() . '/style.css');
+}
--- WP/wp/wp-content/themes/alphateam/header.php
+++ WP/wp/wp-content/themes/alphateam/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 <body class="min-h-screen flex flex-col">
     <header class="bg-white dark:bg-gray-900">
