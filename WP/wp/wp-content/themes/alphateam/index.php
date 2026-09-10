07e5333b0249ad3eb7ca091440fbd41ccd360bec
Edson Galan <edd.galan@hotmail.com>
2026-09-10 00:49:16 -0600

    Display posts dynamically in the index template and add theme screenshot

--- WP/wp/wp-content/themes/alphateam/index.php
+++ WP/wp/wp-content/themes/alphateam/index.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-    <h2>Body</h2>
+    <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) :
+                the_post();
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            endwhile;
+        endif;
+    ?>
 <?php get_footer(); ?>
