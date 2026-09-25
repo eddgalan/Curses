a317b9ac60c3bf87e49bd92d9c8f121577b41d73
Edson Galan <edd.galan@hotmail.com>
2026-09-25 00:54:16 -0600

    Curso API REST Laravel | Planificacion y configuracion de API (Version 2)

--- laravel-api-rest/routes/api.php
+++ laravel-api-rest/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostControllerV1;
+use App\Http\Controllers\Api\V2\PostController as PostControllerV2;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', PostController::class)
+Route::apiResource('v1/posts', PostControllerV1::class)
+    ->only(['index', 'show', 'destroy']);
+
+Route::apiResource('v2/posts', PostControllerV2::class)
     ->only(['index', 'show', 'destroy']);
