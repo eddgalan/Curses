ea499f6d1a487cb742e7f37eb055f88e0402099c
Edson Galan <edd.galan@hotmail.com>
2026-09-24 20:13:34 -0600

    Curso API REST Laravel | Planificacion y configuracionde una API en PHP

---
+++ laravel-api-rest/routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\Api\V1\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('v1/posts', PostController::class)->only('show');
--- laravel-api-rest/routes/web.php
+++ laravel-api-rest/routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index']);
+
+require __DIR__.'/api.php';
