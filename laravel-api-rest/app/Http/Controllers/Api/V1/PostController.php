699f5bf66a1ddf6897e8dcddbf3af656062fa3b3
Edson Galan <edd.galan@hotmail.com>
2026-09-25 00:17:03 -0600

    Curso API REST Laravel | Configuracion de colecciones y paginacion en Laravel

--- laravel-api-rest/app/Http/Controllers/Api/V1/PostController.php
+++ laravel-api-rest/app/Http/Controllers/Api/V1/PostController.php
@@ -6,15 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        //
+        return PostResource::collection(Post::latest()->paginate());
     }
 
     /**
