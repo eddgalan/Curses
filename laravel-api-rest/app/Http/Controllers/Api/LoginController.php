ee0b4da82bcb46924d9fb4e6c62e7e0586eca6b8
Edson Galan <edd.galan@hotmail.com>
2026-09-25 01:46:24 -0600

    Curso API REST Laravel | Autenticacion de Usuarios

---
+++ laravel-api-rest/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required',
+        ]);
+    }
+}
--- laravel-api-rest/routes/api.php
+++ laravel-api-rest/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\V1\PostController as PostControllerV1;
 use App\Http\Controllers\Api\V2\PostController as PostControllerV2;
 use Illuminate\Support\Facades\Route;
+
+Route::post('login', [LoginController::class, 'login']);
 
 Route::apiResource('v1/posts', PostControllerV1::class)
     ->only(['index', 'show', 'destroy'])
