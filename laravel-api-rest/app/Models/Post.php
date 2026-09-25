9483ae9d9b3a63f4db825e21f8c1fe0afb70bb25
Edson Galan <edd.galan@hotmail.com>
2026-09-24 19:53:07 -0600

    Curso API REST Laravel | Diseno web con Tailwind CSS y Laravel

--- laravel-api-rest/app/Models/Post.php
+++ laravel-api-rest/app/Models/Post.php
@@ -5,8 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    /**
+     * @return string
+     */
+    public function getExcerptAttribute(): string
+    {
+        return substr($this->content, 0, 100);
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getPublishedAtAttribute(): string
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
