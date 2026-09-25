24b5deda0d3322fee27d5f5d34cc92873795d1a4
Edson Galan <edd.galan@hotmail.com>
2026-09-25 02:08:37 -0600

    Curso API REST Laravel | Configuracion de login manual en API - Curl de ejemplo: curl --location 'http://laravel-api-rest.test/api/v2/posts' \                    --header 'Authorization: Bearer 4|xjA01Vj45JSh9cnvUXRNMypE4h2AsygG4mL6iKRF24269513' \                    --header 'Cookie: laravel-api-rest-session=eyJpdiI6IlRNUmNrN1N3a3ZJQzY2YllPdE5oOHc9PSIsInZhbHVlIjoib0hyZ3BMdDU1dld1SWt6UDUvYWh1ZklTN1hvNHhOQm8vK3lrNTdRNEg1bnEzVG1oeFdMcjQ3MGxmakRsWm94amNmcHladHJxaWQ1bmN5eEtLbE1qZ2FtbWFjYjA0ektiSWdmMmF1eG5ZdjNjZGp6UDhMTDNuWmd5RHFTUDBzcGoiLCJtYWMiOiJmMmNjOTRmMWQ5NGMyMDk0YTkzZjhkYTYxN2ZhNmIwZjE2ZWYzNzI2OGVkOWY4NDJjYTAyZmZhYTFhNTU1YzM3IiwidGFnIjoiIn0%3D'

--- laravel-api-rest/app/Http/Controllers/Api/LoginController.php
+++ laravel-api-rest/app/Http/Controllers/Api/LoginController.php
@@ -4,17 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    /**
+     * Handles user login by validating credentials and generating an authentication token on success.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
     {
         $this->validateLogin($request);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => Auth::user()->createToken($request->name)->plainTextToken,
+                'message' => 'Logged in successfully'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
 
-    protected function validateLogin(Request $request)
+    /**
+     * Validates the login request by ensuring the required fields are provided and meet the specified criteria.
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function validateLogin(Request $request): array
     {
         return $request->validate([
             'email' => 'required|email',
--- laravel-api-rest/routes/api.php
+++ laravel-api-rest/routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\V1\PostController as PostControllerV1;
 use App\Http\Controllers\Api\V2\PostController as PostControllerV2;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [LoginController::class, 'login']);
+Route::post('login', [LoginController::class, 'login'])->name('login');
 
 Route::apiResource('v1/posts', PostControllerV1::class)
     ->only(['index', 'show', 'destroy'])
